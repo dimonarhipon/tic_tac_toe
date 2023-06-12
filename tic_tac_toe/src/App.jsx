c6e41b4446e2d90e0353cb77dfce974aafc04032
D_arh <dimonarhipon@mail.ru>
2023-06-12 10:54:40 +0300

    added base logic

--- tic_tac_toe/src/App.jsx
+++ tic_tac_toe/src/App.jsx
@@ -1,34 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
+const SYMBOL_X = 'X';
+const SYMBOL_O = 'O';
+const CEIL_MAX = 9;
+
+const computeWinner = (cells) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      cells[a] &&
+      cells[a] === cells[b] &&
+      cells[a] === cells[c]
+    ) {
+      return [a, b, c]
+    }
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentValue, setCurrentValue] = useState(SYMBOL_X);
+  const [ceils, setCeils] = useState(Array(CEIL_MAX).fill(null));
+  const [winnerSequene, setWinnerSequene] = useState(undefined);
+
+  const handleCeilsClick = (index) => {
+    if (ceils[index] || winnerSequene) return;
+
+    const copyCeils = ceils.slice();
+    copyCeils[index] = currentValue;
+    setCeils(copyCeils);
+
+    const winner = computeWinner(copyCeils);
+    setCurrentValue(currentValue === SYMBOL_X ? SYMBOL_O : SYMBOL_X);
+    setWinnerSequene(winner);
+    console.log(winnerSequene)
+  }
+
+  const handleResetClick = () => {
+    setWinnerSequene(undefined);
+    setCeils(Array(CEIL_MAX).fill(null));
+    setCurrentValue(SYMBOL_X);
+  }
+
+  const isDraw = !winnerSequene && ceils.filter(value => value).length === CEIL_MAX;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="">
+      <div className="">
+        {isDraw ? 'Ничья' : winnerSequene ? `Победили ` : `Ход `}
+        {!isDraw && currentValue}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='ceils'>{ceils.map((item, index) => {
+        const isWinner = winnerSequene?.includes(index);
+        return (
+          <button
+            className={`ceil ${isWinner ? 'ceil--winner' : ''}`}
+            key={index}
+            onClick={() => handleCeilsClick(index)}
+          >
+            <span>{item}</span>
+          </button>
+        )
+      })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button onClick={handleResetClick}>Начать сначала</button>    
+    </div>
   )
 }
 
